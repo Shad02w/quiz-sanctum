297d01d19f7c61cce70af49c0e09696c5cee8abb
Shad02w@Pro13 <alvistse123@gmail.com>
2021-02-16 01:49:09 +0800

    questions resource auth

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::apiResource('questions', QuestionController::class);
 Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('questions', QuestionController::class);
 });
